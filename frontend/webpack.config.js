04498f833a22da94b64147adabea1718d709adb3
Robin Blanchard <robin.blanchard00@gmail.com>
2020-04-15 15:15:58 +0200

    Installed babel and added webpack rule

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -18,6 +18,16 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader','css-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env','@babel/preset-react']
+          }
+        }
       }
     ]
   },
